b37ee84ea91288384b7f4b5b173c1ca97c650d94
Ivan Hernandez <ivanbigfoot@gmail.com>
2021-04-18 19:21:23 -0700

    commit 18/abril/2021

---
+++ MatricesDinamicas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MatricesDinamicas{
+    public static void main(String args[]){
+        int filas=0, columnas=0, contador=1;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("¿Cuántas filas desdeas?");
+        filas = teclado.nextInt();
+        System.out.println("¿Cuántas columnas desdeas?");
+        columnas = teclado.nextInt();
+
+        int numeros [][] = new int [filas][columnas];
+        
+    }
+}
