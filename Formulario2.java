aff020f9f6f09c63e82b307cc370438b6e1d6d94
Ivan Medrano <p3yn@Hackintosh-Mackbook-Pro.local>
2021-04-20 23:08:09 -0700

    formulario2

---
+++ Formulario2.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swng.*;
+
+public class Formulario2 extends JFrame{
+    
+    public Formulario2(){
+        setLayout(null);
+    }
+    public static void main(String[] args) {
+        Formulario2 formulario1 = new Formulario2();
+        formulario1.setBounds(0, 0, 400, 550);
+        formulario1.setVisible(true);
+        formulario1.setLocationRelativeTo(null);
+        formulario1.setResizable(false);
+    }
+}
